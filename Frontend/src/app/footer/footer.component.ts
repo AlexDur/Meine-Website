36713f19303d96339361ac584e071a668c24c2ee
Alusch <alexdurach@gmail.com>
2024-01-13 09:20:13 +0100

    Header angepasst

--- Frontend/src/app/footer/footer.component.ts
+++ Frontend/src/app/footer/footer.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {NgIf} from '@angular/common';
+import {Subscription} from 'rxjs';
+import {TranslateService} from '../shared/services/translate.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  loaded: boolean = false;
+  private subscription: Subscription | null = null;
 
+
+  constructor(private translateService: TranslateService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.subscription = this.translateService.areTranslationsLoaded().subscribe(loaded => {
+      this.loaded = loaded;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
