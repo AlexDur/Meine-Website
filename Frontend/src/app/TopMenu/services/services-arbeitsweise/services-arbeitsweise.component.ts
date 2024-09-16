9167cc9f92fecb974819ad986016e80fa81b3831
Alusch <alexdurach@gmail.com>
2024-09-16 19:19:04 +0200

    Nachbesserungen für mobile Darstellung

--- Frontend/src/app/TopMenu/services/services-arbeitsweise/services-arbeitsweise.component.ts
+++ Frontend/src/app/TopMenu/services/services-arbeitsweise/services-arbeitsweise.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import {AccordionModule} from 'primeng/accordion';
+import {AsyncPipe} from '@angular/common';
+import {SharedModule} from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-services-arbeitsweise',
   standalone: true,
   imports: [
-    AccordionModule
+    AccordionModule,
+    AsyncPipe,
+    SharedModule
   ],
   templateUrl: './services-arbeitsweise.component.html',
   styleUrl: './services-arbeitsweise.component.scss'
